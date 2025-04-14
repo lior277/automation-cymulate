8f3e4c65cccd27bba24fe024f3f7674cde92ba2c
lior <lior277@gmail.com>
2025-04-14 19:00:35 +0300

    fix

--- infrastructure/infra/dal/data_reposetory/data_rep.py
+++ infrastructure/infra/dal/data_reposetory/data_rep.py
@@ -3,4 +3,4 @@ class DataRep:
     cymulate_url = "https://app.cymulate.com/"
     user_name = "candidate_user@cymulate1.com"
     password = "ZzAa!@#$43212"
-    time_to_wait_from_seconds = 10
+    time_to_wait_from_seconds = 30
